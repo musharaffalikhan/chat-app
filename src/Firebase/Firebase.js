b76a0df859b4ccc037c86e22bd13d8003e0d2082
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-03-14 00:29:13 +0530

    initialised firebase

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app"
+
+const config = {
+    apiKey: "AIzaSyD8BBHKDJbzMlT2_pj7LnzzNPibIwU16fU",
+  authDomain: "chat-web-app-fccba.firebaseapp.com",
+  databaseURL: "https://chat-web-app-fccba-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-fccba",
+  storageBucket: "chat-web-app-fccba.appspot.com",
+  messagingSenderId: "711775084307",
+  appId: "1:711775084307:web:cdcc4ea0822ed583b744e6"
+};   
+
+const app = initializeApp(config);
