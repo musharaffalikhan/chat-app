059e75826a9c85936079f58806562f866e0cece3
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-03-24 00:05:19 +0530

    Fixes

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch } from "react-router-dom";
 import { ProfileProvider } from "./Auth/AuthContext";
 import PrivateRoute from "./Components/PrivateRoute";
 import PublicRoute from "./Components/PublicRoute";
-// import PublicRoute from "./Components/PublicRoute";
 
 //Pages
 import Home from "./Pages/Home";
