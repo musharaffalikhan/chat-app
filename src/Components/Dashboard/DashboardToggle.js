98519ee1db9d5e7dca728ec048a237e439404781
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-03-24 20:08:53 +0530

    added responsive drawer using media query

--- src/Components/Dashboard/DashboardToggle.js
+++ src/Components/Dashboard/DashboardToggle.js
@@ -1,11 +1,13 @@
 import { Dashboard } from "@rsuite/icons";
 import React from "react";
 import { Button, Drawer } from "rsuite";
+import { useMediaQuery } from "../../Hooks/usemediaquery";
 import { UseModalState } from "../../Hooks/UseModalState";
 import DashBoard from "./DashBoard";
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = UseModalState();
+  const isMobile = useMediaQuery("(max-width: 992px)");
   return (
     <>
       <Button
@@ -17,7 +19,7 @@ const DashboardToggle = () => {
       >
         Dashboard
       </Button>
-      <Drawer placement="left" onClose={close} open={isOpen}>
+      <Drawer full={isMobile} placement="left" onClose={close} open={isOpen}>
         <DashBoard />
       </Drawer>
     </>
---
+++ src/Hooks/usemediaquery.js
@@ -0,0 +1,22 @@
+// usage
+// const is992px = useMediaQuery('(max-width: 992px)')
+
+import { useEffect, useState } from "react";
+
+export const useMediaQuery = query => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    const listener = evt => setMatches(evt.matches);
+
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
